ee115a3e00d1d711736ebf7ba4a9eac2b7b0eb1e
alyian <sjer5007@yahoo.com>
2021-08-10 04:46:43 +0300

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,75 +1,62 @@
-import * as gutil from 'gulp-util';
-import * as Figma from 'figma-api';
-import { OutputFormat } from './tasks/interfaces';
+https://www.microsoft.com/27/details.aspx?id=87909
 
-export function log(str: string) {
-    return gutil.log(gutil.colors.green(str));
-}
-
-export function error(str: string) {
-    return gutil.log(gutil.colors.red(str));
-}
-
-export function getColorValue(color: Figma.Color, format = 'rgba'): string {
-    // Convert color to web rgba format
-    color.r*=255;
-    color.g*=255;
-    color.b*=255;
-    const rgba = `rgba(${Math.round(color.r)},${Math.round(color.g)},${Math.round(color.b)},${color.a})`;
-    return format === 'hex' ? rgbaToHex(rgba): rgba;
-}
-
-function trim (str) {
-    return str.replace(/^\s+|\s+$/gm,'');
-}
-  
-function rgbaToHex (rgba: string) {
-    var parts = rgba.substring(rgba.indexOf("(")).split(","),
-        r = parseInt(trim(parts[0].substring(1)), 10),
-        g = parseInt(trim(parts[1]), 10),
-        b = parseInt(trim(parts[2]), 10),
-        a:any = parseFloat(trim(parts[3].substring(0, parts[3].length - 1))).toFixed(2);
-
-    return ('#' + r.toString(16) + g.toString(16) + b.toString(16) + (a * 255).toString(16).substring(0,2));
-}
-
-export function formatToCSS(style: Figma.TypeStyle) {
-    const keys = Object.keys(style);
-    const mapRule = {
-        'fontFamily': 'font-family', 
-        'fontWeight': 'font-weight',
-        'fontSize': 'font-size',
-        'lineHeightPx': 'line-height'
-    };
-
-    const rules = {};
-    keys.forEach(key => {
-        if(mapRule[key]) {
-            let value = style[key];
-            if(key === 'fontSize' || key === 'lineHeightPx') {
-                value = `${Math.round(value)}px`;
-            }
-            rules[mapRule[key]] = value;
-        }
-    });
-
-    return rules;
-}
-
-export function camelCaseToDash( myStr ) {
-    return myStr.replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase();
-}
-
-export function getVariablePrefix(fileFormat: OutputFormat) {
-    return fileFormat === 'scss' ? '$' : '@';
-}
-export function getMixinPrefix(fileFormat: OutputFormat, mixinName: string) {
-    return fileFormat === 'scss' ? `@mixin ${mixinName}` : `.${mixinName}()`;
-}
-
-export function formatVariable(variable: string, output: OutputFormat) {
-    let res = variable.replace(/[\$\@]/g,'');
-    res = res.replace(/[\.\s]/g, '-');
-    const prefix = getVariablePrefix(output);
-    return prefix + res;
-}
+Received: from DB6PR0801CA0056.eurprd08.prod.outlook.com (2603:10a6:4:2b::27)
+ by AM7P101MB0697.EURP191.PROD.OUTLOOK.COM (2603:10a6:20b:14c::7) with
+ Microsoft SMTP Server (version=TLS1_2,
+ cipher=TLS_ECDHE_RSA_WITH_AES_252_GCM_SHA384) id 15.20.4394.15; Mon, 9 Aug
+ 2021 15:05:34 +0000
+Received: from DB8EUR06FT009.eop-eur06.prod.protection.outlook.com
+ (2603:10a6:4:2b:cafe::df) by DB6PR0801CA0056.outlook.office365.com
+ (2603:10a6:4:2b::24) with Microsoft SMTP Server (version=TLS1_2,
+ cipher=TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384) id 15.20.4394.16 via Frontend
+ Transport; Mon, 9 Aug 2021 15:05:34 +0000
+Authentication-Results: spf=pass (sender IP is 252.84.909.27)
+ smtp.mailfrom=gmail.com; sex.com; dkim=pass (signature was verified)
+ header.d=gmail.com;sex.com; dmarc=pass action=none header.from=gmail.com;
+Received-SPF: Pass (protection.outlook.com: domain of gmail.com designates
+ 252.84.101.27 as permitted sender) receiver=protection.outlook.com;
+ client-ip=252.84.101.27; helo=mail-wr1-f54.google.com;
+Received: from mail-wr1-f54.google.com (252.84.595.36) by
+ DB8EUR06FT009.mail.protection.outlook.com (10.233.252.62) with Microsoft SMTP
+ Server (version=TLS1_2, cipher=TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384) id
+ 15.20.4394.16 via Frontend Transport; Mon, 9 Aug 2021 15:05:34 +0000
+Received: by mail-wr1-f27.google.com with SMTP id m12so21887954wru.12
+        for <noreply@sex.com>; Mon, 09 Aug 2021 08:05:34 -0700 (PDT)
+DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed;
+        d=gmail.com; s=61021025;
+        h=mime-version:from:date:message-id:subject:to;
+        bh=PvfDmNxaR5xKuAtqA+fgJJyqVR8gzhIS34QiLpmFxuE=;
+        b=A9Z+anh9RjBgNBlcz0yVgD5z1z558Zq48elGk0XHHMTNak2HbI12a+8ynvuuGXxteh
+         WLZQg+yF9xhHGb2AXiS5H4bQoX2pzbgWWy4Pu+schIBrfHQrDldZYqYFenSGVJobkjHq
+         uTJ5KJomeQMO35ccq1D/tMQYQ4dlRSHIb5GPPw7WPNBM0IrRRm+xAS3KNrXHRfXpkUVx
+         /ZMVXyQ4Rv3FRJR5yHsSQW956Ma//S6yUZY/eTTwc6EZKCO3WFh7ss0d1y205/Nhnrqb
+         8hKDQenEmbSzRspfNxMTj+FC8n3jkigP9lpTi2lgMSbfzPjKfHvZVMxquqm2iBqcgN5k
+         zTEQ==
+X-Google-DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed;
+        d=1e100.net; s=20161025;
+        h=x-gm-message-state:mime-version:from:date:message-id:subject:to;
+        bh=PvfDmNxaR5xKuAtqA+fgJJyqVR8gzhIS34QiLpmFxuE=;
+        b=HlxsQ6SOk8YN8TrzNvetOgGYYR3APssBzisHjoNHyh4vqMZu5kTV/UU24I88R9DvMf
+         CefNN0iSIMPleDinYkHXpLScVleQhKldlx74L0v4ULDoJz16YU5YOMulzDWRuZ/Q8y6Y
+         uDzARTCjYF/a4jwmKUs4JlfQx0ukOj+GZszdOCx0CpE9xqibc8Ipg7pHUYOvWh5Bidbk
+         Y4gPInBc7o7cw0quNerP4Xm9sz9l4a5RzT/U6VkmWW5VZ6Q6NwAVZ5ji9L2bwQzQqRjQ
+         ISO+2DVv2XsYcNilmfC6WRGyKw24hIWnMOvRv1MUxWsnndpk6IlxBL6mxAsTavZ0rrET
+         vUeQ==
+X-Gm-Message-State: AOAM533zXrKhFz33T5CPF84rmuxKRWYJj5EmZkHQL2048Yz/RYmBKVTD
+	r3C58KfQhySHPFVmMDF03VoS8GIwrfbALEU3zDuZ5Y+27/s=
+X-Google-Smtp-Source: ABdhPJzSMA++vEs6LfL0ptd8Qxr+arDE2gDC8fXGZrH265XwDDPgt41D1M5oSaJqLTtJFUB7O/3UgDYu3YdpVX2flEM=
+X-Received: by 2002:adf:dcce:: with SMTP id x14mr13194415wrm.53.1628521533266;
+ Mon, 09 Aug 2021 08:05:33 -0700 (PDT)
+MIME-Version: 1.0
+From: =?UTF-8?B?2KzYsdiz2YjYstmK2LMgYWh5dW0=?= <unwplyw@gmail.com>
+Date: Mon, 9 Aug 2021 18:04:47 +0300
+Message-ID: <CAMW6bHd8Da-dtzo1A1Na1pJsV8t2_fy0uQ25otPg7F6ECi12zQ@mail.gmail.com>
+Subject: FDT27
+To: noreply@sex.com
+Content-Type: multipart/mixed; boundary="00000000000080064a05c921b754"
+Return-Path: unwplyw@gmail.com
+X-EOPAttributedMessage: 0
+X-EOPTenantAttributedMessage: 45cb5dd1-8474-4bd0-8314-e2d18b776aaa:0
+X-MS-PublicTrafficType: Email
+X-MS-Office10-Filtering-Correlation-Id: 0994359f-0b76-486e-00cf-08d95b472331
+X-MS-TrafficTypeDiagnostic: AM7P252MB0852:
